7bac900bf82639a929d18653ab56f6bb9f18c107
yukta90 <72794968+yukta90@users.noreply.github.com>
2026-05-02 12:41:16 +0530

    fix: handle server-side errors and improve error responses

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const {listingSchema} = require("./schema.js");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
@@ -23,12 +26,28 @@ async function main() {
     await mongoose.connect(mongoURL);
 }
 
+const validateListing = (req, res, next) => {
+    let {error} = listingSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, error);
+    }else{
+        next();
+    }
+};
+
+
+// Root Route
+app.get("/", (req, res) =>{
+    res.send("Hi! I am root");
+});
+
 
 // Index Route (To display entire list)
-app.get("/listings",  async(req, res) =>{
+app.get("/listings",  wrapAsync(async(req, res) =>{
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", {allListings});
-});
+}));
 
 
 // Add Route (Form to add new stay)
@@ -36,47 +55,52 @@ app.get("/listings/new", (req, res) =>{
     res.render("listings/new.ejs");
 });
 // (To add new stay in DB)
-app.post("/listings", async(req, res) =>{
+app.post("/listings", validateListing, wrapAsync(async(req, res, next) =>{
     const newListing = new Listing(req.body.listing);
     await newListing.save();
-    console.log(newListing);
     res.redirect("/listings");
-});
+}));
 
 
 // Show Route (To display specific stay)
-app.get("/listings/:id", async(req, res) =>{
+app.get("/listings/:id", wrapAsync(async(req, res) =>{
     let {id} = req.params;
     const specificListing = await Listing.findById(id);
     res.render("listings/show.ejs", {specificListing});
-});
+}));
 
 
 // Update Route (Form to edit a stay)
-app.get("/listings/:id/edit", async(req, res) =>{
+app.get("/listings/:id/edit", wrapAsync(async(req, res) =>{
     let {id} = req.params;
     const editListing = await Listing.findById(id);
     res.render("listings/edit.ejs", {editListing});
-});
+}));
 // Update edited details in DB
-app.put("/listings/:id", async(req, res) =>{
-    let {id} = req.params;
+app.put("/listings/:id", validateListing, wrapAsync(async(req, res) => {
+    let {id} = m/req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing}, {runValidators: true});
     res.redirect(`/listings/${id}`);
-});
+}));
 
 
 // Delete Route (To delete specific stay)
-app.delete("/listings/:id", async(req, res) =>{
+app.delete("/listings/:id", wrapAsync(async(req, res) =>{
     let {id} = req.params;
     await Listing.findByIdAndDelete(id);
     res.redirect("/listings");
+}));
+
+
+app.use((req, res, next) =>{
+    next(new ExpressError(404, "Page not found!"));
+    
 });
 
-
-// Root Route
-app.get("/", (req, res) =>{
-    res.send("Hi! I am root");
+// Error Handling Middleware
+app.use((err, req, res, next) =>{
+    let {statusCode = 500, message = "Something went wrong!"} = err;
+    res.status(statusCode).render("error.ejs",{message});
 });
 
 //Server Connection code
