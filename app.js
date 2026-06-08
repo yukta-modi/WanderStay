b47432d52fb2a576258cc69796428cc331190e8b
yukta modi <yukta7347@gmail.com>
2026-06-08 12:39:28 +0530

    feat: implement review deletion and cascade delete on listing removal

--- app.js
+++ app.js
@@ -68,6 +68,7 @@ app.get("/listings/new", (req, res) =>{
 // (To add new stay in DB)
 app.post("/listings", validateListing, wrapAsync(async(req, res, next) =>{
     const newListing = new Listing(req.body.listing);
+    console.log(newListing);
     await newListing.save();
     res.redirect("/listings");
 }));
